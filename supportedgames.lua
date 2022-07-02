997b348a618de7586e4d0e55ab522f420ac4bf43
capybara god <108592411+gingercoreinnit@users.noreply.github.com>
2022-07-02 23:49:08 +0300

    Create supportedgames.lua

---
+++ supportedgames.lua
@@ -0,0 +1,14 @@
+local SupportedGames = {
+    ["Nen Fighting Simulator"] = 9555732501,
+    ["Smash Legends"] = 5630129588,
+    ["Saber Simulator"] =  3823781113,
+    ["Animal Simulator"] = 5712833750,
+    ["Tornado Simulator"] = 9300344892,
+    ["Speed Simulator 2"] = 7018634745,
+    ["Gym Training Simulator"] = 9681073174,
+    ["Ninja Legends"] = 3956818381,
+    ["Clicker Madness"] = 5490351219,
+    ["Tapping Legends X"] = 8750997647,
+    ["Food Magnet Simulator"] = 6544175281,
+    ["Magic Champions"] = 8097148487
+}
